54d5f92d97caccd3e6c85bce040af95d60de311c
hatimm <hatimmeftahi90@gmail.com>
2026-02-20 08:02:26 +0000

    fix issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://aidevdaily.com',
+  site: 'https://aidevdaily.com/',
   output: 'static',
   integrations: [sitemap()],
 });
